2e59ad279a3dfb87bba78067343ac9ce20df4e60
虜漣踪 <lorenzo.bellier@gmail.com>
2026-01-19 16:40:26 +0100

    Add script to backup home directories of all users

--- scripts/backup_users.sh
+++ scripts/backup_users.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+# Backup home directories of all users
 
+BACKUP_DIR="/var/backups/home"
+DATE=$(date +%F)
+mkdir -p $BACKUP_DIR
+
+for user in $(ls /home); do
+  tar -czf $BACKUP_DIR/${user}_backup_$DATE.tar.gz /home/$user
+done
+
+echo "Backup completed for all users."
