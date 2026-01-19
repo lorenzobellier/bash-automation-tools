9b2cc186b2736f80eb00f3ea0a6107c4f80bd648
虜漣踪 <lorenzo.bellier@gmail.com>
2026-01-19 16:41:27 +0100

    Add script to check status of key services

--- scripts/check_services.sh
+++ scripts/check_services.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+# Check status of important services
 
+SERVICES=("ssh" "apache2" "mysql")
+for service in "${SERVICES[@]}"; do
+  systemctl is-active --quiet $service && echo "$service is running" || echo "$service is NOT running"
+done
